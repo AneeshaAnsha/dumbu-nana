8c8d59f3bafba518527b590b307cc2ace2f56fa5
14759 <aneeshaansha01@gmail.com>
2023-03-12 15:51:15 -0400

    Hello Ansha

---
+++ Fluffy/src/test/java/com/guideme/Fluffy/FluffyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guideme.Fluffy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluffyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
